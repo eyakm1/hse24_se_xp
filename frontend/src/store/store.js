b9a4ffe738add127c5da86d2bd2ef79ee91d01e4
eyakm1 <eyakm1@protonmail.com>
2024-10-28 13:36:26 +0300

    Student assigments list

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import assignmentsReducer from './slices/assignmentsSlice';
 
 export const store = configureStore({
   reducer: {
